7de354a27e202784dbc49c9f1333031ad93c9096
FMR7 <14892931+FMR7@users.noreply.github.com>
2025-05-06 01:43:38 +0200

    error control

--- src/container/containerService.js
+++ src/container/containerService.js
@@ -1,45 +1,45 @@
 const { exec } = require('child_process');
 
 function getStatus(containerId) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         try {
             exec(`docker inspect -f '{{.State.Running}}' ${containerId}`, (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Error checking status: ${error.message}`);
-                    throw new Error('Error checking container status');
+                    return reject(new Error('Error checking container status'));
                 }
                 if (stderr) {
                     console.error(`Stderr: ${stderr}`);
-                    throw new Error('Error checking container status');
+                    return reject(new Error('Error checking container status'));
                 }
 
                 return resolve(stdout.trim().replace("\n", "") === 'true');
             });
         } catch (err) {
             console.error(`Error checking status: ${err.message}`);
-            throw new Error('Error checking container status');
+            return reject(new Error('Error checking container status'));
         }
     });
 }
 
 function getName(containerId) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         try {
             exec(`docker inspect -f '{{.Name}}' ${containerId}`, (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Error checking status: ${error.message}`);
-                    throw new Error('Error checking container name');
+                    return reject(new Error('Error checking container name'));
                 }
                 if (stderr) {
                     console.error(`Stderr: ${stderr}`);
-                    throw new Error('Error checking container name');
+                    return reject(new Error('Error checking container name'));
                 }
 
                 return resolve(stdout.trim().replace("\n", "").replace("/", ""));
             });
         } catch (err) {
             console.error(`Error checking name: ${err.message}`);
-            throw new Error('Error checking container name');
+            return reject(new Error('Error checking container name'));
         }
     });
 }
