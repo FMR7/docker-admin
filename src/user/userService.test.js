d1685a5f54cb35edf751541097b8a4a097ec6417
FMR7 <14892931+FMR7@users.noreply.github.com>
2025-05-07 01:37:19 +0200

    TEST - Service signin

--- src/user/userService.test.js
+++ src/user/userService.test.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const userService = require('./userService');
 const usuarioRepo = require('./userRepository');
 jest.mock('./userRepository');
@@ -67,6 +68,46 @@ describe('getUserByUsernameAndActive', () => {
     usuarioRepo.findByUsernameAndActive = jest.fn().mockRejectedValue(new Error('Database error'));
 
     await expect(userService.getUserByUsernameAndActive('test')).rejects.toThrow('Database error');
+  });
+});
+
+describe('signin', () => {
+  it('should return a user without password', async () => {
+    const testPasswordHash = (await bcrypt.hash(testPassword, 10)).toString();
+    usuarioRepo.findByUsernameAndActive = jest.fn().mockResolvedValue({ username: 'test', active: true, admin: false, password: testPasswordHash, password_wrong_tries: 0 });
+    usuarioRepo.updatePasswordWrongTries = jest.fn().mockResolvedValue({ username: 'test', active: true, admin: false, password: testPasswordHash, password_wrong_tries: 0 });
+
+    const res = await userService.signin('test', testPassword);
+    expect(res).toEqual({ username: 'test', active: true, admin: false });
+  });
+
+  it('should return undefined if no user is found', async () => {
+    usuarioRepo.findByUsernameAndActive = jest.fn().mockResolvedValue(undefined);
+
+    await expect(userService.signin('test', testPassword)).rejects.toThrow('User not found or inactive');
+  });
+
+  it('should handle errors', async () => {
+    usuarioRepo.findByUsernameAndActive = jest.fn().mockRejectedValue(new Error('Database error'));
+
+    await expect(userService.signin('test', testPassword)).rejects.toThrow('Database error');
+  });
+
+  it('should throw an error if password is invalid', async () => {
+    const testPasswordHash = (await bcrypt.hash(testPassword, 10)).toString();
+    usuarioRepo.findByUsernameAndActive = jest.fn().mockResolvedValue({ username: 'test', active: true, admin: false, password: testPasswordHash, password_wrong_tries: 0 });
+    usuarioRepo.updatePasswordWrongTries = jest.fn().mockResolvedValue({ username: 'test', active: true, admin: false, password: testPasswordHash, password_wrong_tries: 1 });
+
+    await expect(userService.signin('test', 'wrongPassword')).rejects.toThrow('Invalid password');
+  });
+  
+  it('should disable user after 3 failed attempts', async () => {
+    const testPasswordHash = (await bcrypt.hash(testPassword, 10)).toString();
+    usuarioRepo.findByUsernameAndActive = jest.fn().mockResolvedValue({ username: 'test', active: true, admin: false, password: testPasswordHash, password_wrong_tries: 3 });
+    usuarioRepo.updatePasswordWrongTries = jest.fn().mockResolvedValue({ username: 'test', active: true, admin: false, password: testPasswordHash, password_wrong_tries: 4 });
+    usuarioRepo.disableUser = jest.fn().mockResolvedValue({ username: 'test', active: false, admin: false });
+
+    await expect(userService.signin('test', 'wrongPassword')).rejects.toThrow('User disabled due to too many failed login attempts. Contact admin.');
   });
 });
 
