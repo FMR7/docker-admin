496a71e3f58be9aa29f96459b85ee65401e095a3
FMR7 <14892931+FMR7@users.noreply.github.com>
2025-05-09 01:10:34 +0200

    DB Script

---
+++ src/config/initDB.sql
@@ -0,0 +1,18 @@
+CREATE TABLE public.usuarios (
+	username varchar(20) NOT NULL,
+	"password" varchar(512) NOT NULL,
+	active bool DEFAULT false NOT NULL,
+	password_wrong_tries int4 DEFAULT 0 NOT NULL,
+	"admin" bool DEFAULT false NOT NULL,
+	CONSTRAINT usuarios_pk PRIMARY KEY (username)
+);
+
+CREATE TABLE public.log (
+	log_key bigserial NOT NULL,
+	username varchar(20) NOT NULL,
+	"action" varchar NOT NULL,
+	detail varchar NULL,
+	log_date date DEFAULT now() NOT NULL,
+	CONSTRAINT log_pkey PRIMARY KEY (log_key),
+	CONSTRAINT log_usuarios_fk FOREIGN KEY (username) REFERENCES public.usuarios(username)
+);
