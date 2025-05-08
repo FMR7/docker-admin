8caedfa0c704c00a3d1060e3fd96a15a82f37e9e
FMR7 <14892931+FMR7@users.noreply.github.com>
2025-05-08 22:12:16 +0200

    FIX

--- src/container/containerService.js
+++ src/container/containerService.js
@@ -93,17 +93,17 @@ function turnOffContainer(containerId) {
 function getContainers() {
     return new Promise((resolve, reject) => {
         const containerIds = process.env.CONTAINERS.split(',');
-        const result = { ok: true, containers: [] };
+        const containers = [];
 
         Promise.all(containerIds.map(async (containerId) => {
             if (!containerId) throw new Error('Container ID required');
             const status = await getStatus(containerId);
             const name = await getName(containerId);
-            result.containers.push({ id: containerId, name, status });
+            containers.push({ id: containerId, name, status });
         }))
             .then(() => {
-                console.log(result);
-                resolve(result);
+                console.log(containers);
+                resolve(containers);
             })
             .catch((err) => {
                 reject(new Error('Error getting containers'));
