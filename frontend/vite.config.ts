d52e072de4cd64ff319d1cbb19b3c9468bff4413
FMR7 <14892931+FMR7@users.noreply.github.com>
2025-05-11 00:49:25 +0200

    FIX Warn postCSS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // Vite automáticamente expone variables que empiezan por VITE_
 const isSSL = process.env.VITE_SSL === 'true';
 const apiPort = process.env.VITE_API_PORT || (3000);
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
