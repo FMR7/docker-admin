5a246384183cb5b21eb05f1de653f48dbce91dd1
FMR7 <14892931+FMR7@users.noreply.github.com>
2025-05-17 17:09:38 +0200

    Read Only Container ID

--- frontend/src/components/TextInput.jsx
+++ frontend/src/components/TextInput.jsx
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
 
-const TextInput = ({ id, label, type = 'text', ...props }) => {
+const TextInput = ({ id, label, type = 'text', readOnly = false, ...props }) => {
+  const baseClass = 'input input-lg text-center w-full px-3 py-2 my-1 rounded border border-base-300 dark:border-base-700 text-base-content dark:text-base-content';
+  const bgClass = readOnly ? 'bg-base-300 dark:bg-base-700' : 'bg-base-100 dark:bg-base-700';
+
+
   return (
     <input
       type={type}
       id={id}
       name={id}
       placeholder={label}
-      className="input input-lg text-center w-full px-3 py-2 my-1 rounded border border-base-300 dark:border-base-700 bg-base-100 dark:bg-base-700 text-base-content dark:text-base-content"
+      readOnly={readOnly}
+      className={`${baseClass} ${bgClass}`}
       {...props}
     />
   );
@@ -17,6 +22,7 @@ TextInput.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
+  readOnly: PropTypes.bool
 };
 
 export default TextInput;
